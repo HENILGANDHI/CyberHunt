cba74965a48e226b928cd176839221a40e05e3fa
HENILGANDHI <93908400+HENILGANDHI@users.noreply.github.com>
2022-04-13 21:34:58 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,10 +8,10 @@ go install -v github.com/tomnomnom/gf@latest
 go install -v github.com/tomnomnom/qsreplace@latest
 go install -v github.com/ferreiraklet/airixss@latest
 cd /root/go/bin
-cp subfinder/usr/bin
-cp waybackurls/usr/bin
-cp gf/usr/bin
-cp airixss/usr/bin
-cp qsreplace/usr/bin
+cp subfinder /usr/bin
+cp waybackurls /usr/bin
+cp gf /usr/bin
+cp airixss /usr/bin
+cp qsreplace /usr/bin
 
 echo "Installation completed!"
